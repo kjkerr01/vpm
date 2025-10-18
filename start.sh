1769e76e48da144f4c5848704705c1513b9977a0
kjkerr01 <kainoa1kerr@gmail.com>
2025-10-18 12:29:17 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Start virtual display
+Xvfb :99 -screen 0 1280x720x24 &
+
+# Export display
+export DISPLAY=:99
+
+# Start lightweight desktop
+fluxbox &
+
+# Start VNC server on display
+x11vnc -display :99 -nopw -forever -shared -rfbport 5900 &
+
+# Start noVNC on port 8080
+websockify --web=/usr/share/novnc/ 8080 localhost:5900
